4bf846927af4fee8343ca33f061d3cd1c0b0563c
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-06-30 14:52:20 -0400

    first commit

---
+++ type.py
@@ -0,0 +1,14 @@
+boxer = 'Floyd Mayweather'
+print(boxer, 'is', type(boxer))
+
+kilo =  147
+print(kilo, 'is', type(kilo))
+
+speed = 100
+print(speed, 'is', type(speed))
+
+champ = True
+print(champ, 'is' type(champ))
+
+champ = 5>9
+print(champ, 'is' type(champ))
